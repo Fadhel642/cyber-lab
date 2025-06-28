5a9860db45abffd7d0117c8f13bfdf97fed3af93
Fadhel642 <fadhelg845@gmail.com>
2025-06-28 18:11:21 +0200

    Update infos_sys.sh

--- bash/infos_sys.sh
+++ bash/infos_sys.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # Script : infos_sys.sh
 # Objectif : afficher les informations système de base
+
+echo "=== Infos Système ==="
+echo "Utilisateur : $(whoami)"
+echo "Nom de la machine : $(hostname)"
