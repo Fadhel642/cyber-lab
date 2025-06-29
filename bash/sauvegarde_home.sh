952c222596f1f70cc2faec0926a6402282e3edaa
Fadhel642 <fadhelg845@gmail.com>
2025-06-29 19:14:59 +0200

    Update sauvegarde_home.sh

--- bash/sauvegarde_home.sh
+++ bash/sauvegarde_home.sh
@@ -4,3 +4,6 @@
 # Objectif : Sauvegarder le dossier personnel dans une archive compressée
 
 mkdir -p ~/sauvegardes   # Créer le dossier de destination
+
+DATE=$(date +%F)                       # La date du jour au format AAAA-MM-JJ
+FILENAME="home_backup_$DATE.tar.gz"    # Créer le nom du fichier de sauvegarde
