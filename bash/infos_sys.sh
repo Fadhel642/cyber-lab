46084293f77c899da92aff7a3e29765f6a8bec91
Fadhel642 <fadhelg845@gmail.com>
2025-06-29 03:40:29 +0200

    Update infos_sys.sh

--- bash/infos_sys.sh
+++ bash/infos_sys.sh
@@ -4,4 +4,8 @@
 
 echo -e "\e[34m=== Infos système ===\e[0m"
 echo "Utilisateur : $(whoami)"
-echo "Nom de la machine : $(hostname)"
+echo "Machine : $(hostname)"
+echo "Uptime : $(uptime -p)"
+echo "Mémoire utilisée : $(free -h | grep Mem | awk '{print $3 " / " $2}')"
+echo "Charge CPU : $(uptime | awk -F'load average: ' '{print $2}')"
+echo "Adresse IP locale : $(hostname -I | awk '{print $1}')"
