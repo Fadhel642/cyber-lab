7e26a10ce8e7564c223f2f06b1821afd184b93f2
Fadhel642 <fadhelg845@gmail.com>
2025-07-01 15:10:44 +0200

    Update sauvegarde_home.sh

--- bash/sauvegarde_home.sh
+++ bash/sauvegarde_home.sh
@@ -7,3 +7,8 @@ mkdir -p ~/sauvegardes   # Créer le dossier de destination
 
 DATE=$(date +%F)                       # La date du jour au format AAAA-MM-JJ
 FILENAME="home_backup_$DATE.tar.gz"    # Créer le nom du fichier de sauvegarde
+
+# Créer un archive compressé du dossier personnel, en excluant le dossier sauvegardes
+tar -czf ~/sauvegardes/$FILENAME --exclude=$HOME/sauvegardes $HOME
+
+echo "Sauvegarde créée : $FILENAME"  # Affiche un message de confirmation
