c8de637c5aa70e63e8bb37b685f96992d81a0995
Fadhel642 <fadhelg845@gmail.com>
2025-07-02 02:04:23 +0200

    Update audit_machine.sh

--- projets/audit_machine.sh
+++ projets/audit_machine.sh
@@ -2,3 +2,7 @@
 
 # Script : audit_machine.sh
 # Objectif : Effectuer un audit simple d'une machine Linux 
+
+# 1- Créer le fichier de rapport
+rapport="rapport_audit.txt"
+echo "Rapport d'audit généré le $(date)" > $rapport
