20060ca609e56d44d0ec756f934c3fb3c16aeb4c
Fadhel642 <fadhelg845@gmail.com>
2025-07-02 02:06:05 +0200

    Update audit_machine.sh

--- projets/audit_machine.sh
+++ projets/audit_machine.sh
@@ -6,3 +6,10 @@
 # 1- Créer le fichier de rapport
 rapport="rapport_audit.txt"
 echo "Rapport d'audit généré le $(date)" > $rapport
+
+# 2- Informations système
+echo -e "\n[Infos système]" >> $rapport
+echo "Utilisateur     : $(whoami)" >> $rapport
+echo "Nom de la machine : $(hostname)" >> $rapport
+echo "Uptime          : $(uptime -p)" >> $rapport
+echo "Version noyau   : $(uname -r)" >> $rapport
